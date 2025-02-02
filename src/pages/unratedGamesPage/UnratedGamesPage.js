d065cbf19c20ef494004ff6f68677e94a0b8b3d2
Jhonata Costa <costajhonata8@gmail.com>
2025-02-02 16:46:43 -0300

    #19[feature] - Making game cards for Unrated Games page

--- src/pages/unratedGamesPage/UnratedGamesPage.js
+++ src/pages/unratedGamesPage/UnratedGamesPage.js
@@ -1,59 +1,87 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import './UnratedGamesPage.css';
 
 const UnratedGamesPage = () => {
   const [games, setGames] = useState([]);
+  const [page, setPage] = useState(0);
+  const pageSize = 10;
   const [sortDirection, setSortDirection] = useState("asc");
-  const [searchQuery, setSearchQuery] = useState("");
+  const [gameName, setGameName] = useState("");
+  const navigate = useNavigate();
 
   const backendIP = process.env.REACT_APP_BACKEND_IP;
   const backendPort = process.env.REACT_APP_BACKEND_PORT;
   const apiUrl = `http://${backendIP}:${backendPort}/game`; 
 
   useEffect(() => {
-    axios
-      .get(`http://${apiUrl}/unrated-games`, {
-        params: { pageNumber: 0, pageSize: 10, orderBy: "asc", sortBy: "id" },
-      })
-      .then((response) => {
-        setGames(response.data.content);
-      })
-      .catch((error) => {
-        console.error("Error fetching unrated games", error);
+    fetchGames();
+  }, [page]);
+
+  const fetchGames = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/unrated-games`, {
+        params: {
+          pageNumber: page,
+          pageSize: pageSize,
+          orderBy: "asc",
+          sortBy: "name",
+          gameName: gameName || undefined,
+        },
       });
-  }, []);
+      setGames(response.data.content);
+    } catch (error) {
+      console.error("Failed to fetch Unrated Games", error);
+    }
+  };
 
   const toggleSortDirection = () => {
     setSortDirection((prevDirection) => (prevDirection === "asc" ? "desc" : "asc"));
   };
 
   const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
+    setGameName(event.target.value);
   };
 
   return (
     <div className="unratedpage">
       <div style={{ textAlign: 'center'}}>
 
-        <div class="header">
+        <div class="header-unrated">
           <h1>Rate your Games</h1>
 
-          <div class="searchbarfilter">
+          <div class="searchbarfilter-unrated">
             <input
               type="text"
               placeholder="Search by Name"
-              value={searchQuery}
+              value={gameName}
               onChange={handleSearchChange}
-              class="searchbar"/>
+              class="searchbar-unrated"/>
 
-            <button onClick={toggleSortDirection} class="sortbutton">
+            <button onClick={toggleSortDirection} class="sortbutton-unrated">
               Name: {sortDirection === "asc" ? "Asc" : "Desc"}
             </button>
           </div>
         </div>
+
+        <div class="main-unrated">
+          <div class="cardsArea-unrated">
+            {games.map((game, index) => (
+            
+                <div class="gameCard-unrated" onClick={() => navigate(`${game.id}/rating`)}>
+                  <img
+                    src={game.imageUrl}
+                    alt={`Cover of ${game.name}`}
+                  />
+                </div>  
+            ))}
+          </div>
+        </div>
+
       </div>
     </div>
   );
 };
+
 export default UnratedGamesPage;
