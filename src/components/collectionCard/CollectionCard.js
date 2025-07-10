546c448107e572ba36e5455d9ab002922c9d8b11
Jhonata Costa <costajhonata8@gmail.com>
2025-07-10 17:11:09 -0300

    #68[feature] - Update CollectionCard styles and integrate dynamic background color

--- src/components/collectionCard/CollectionCard.js
+++ src/components/collectionCard/CollectionCard.js
@@ -1,8 +1,12 @@
 import './CollectionCard.css';
 
 const CollectionCard = ({ collection, index }) => {
+    const containerStyle = {
+        '--header-background-color-alt': collection.color || '#e7e7e7'
+    };
+
     return (
-        <div key={index} className="collection-container">
+        <div key={index} className="collection-container" style={containerStyle}>
             <p className="collection-name">{collection.name}</p>
             <p className="collection-count">Games: {collection.games?.length || 0}</p>
             <p className="collection-rating">{collection.averageRating}</p>
